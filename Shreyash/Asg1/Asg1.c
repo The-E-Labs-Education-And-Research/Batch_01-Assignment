41c9e6c6de1f76177f99b3ea991374cff72b7393
Shdhore <156451423+Shdhore@users.noreply.github.com>
2024-01-12 21:41:46 +0530

    #1 first code

---
+++ Shreyash/Asg1/Asg1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define MSSG "HELLO WORLD\n"
+int main()
+{
+    printf(MSSG);
+
+    return 0;
+}
---
+++ Shreyash/Asg3/Asg3.c
@@ -0,0 +1,18 @@
+ //Take a character as an input and print it.
+ #include<stdio.h>
+ int main()
+ { int i; 
+ char chad;
+ printf("give me a name\n");
+ scanf("%c", chad);
+ printf("%c", chad);
+// for(i=0;i<9;i++){
+ //scanf("%c", &chad[i]);
+ //}
+ //for(i=0;i<9;i++){
+ 
+ //printf("%c", chad[i]);
+// }
+ 
+ return 0;
+ }
